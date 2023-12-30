bb74a76d7fc57979a47212e6e1239cd0a9e82520
dhamaoka <xiacelite@gmail.com>
2023-12-31 00:09:38 +0900

    0.0.0.28 主に検索時の処理が重いので、ゴリっと書き換え前のコミット。

--- MovieInfo.cs
+++ MovieInfo.cs
@@ -6,15 +6,15 @@ namespace IndigoMovieManager
     public class MovieInfo
     {
         private long movie_id = 0;
-        private readonly string movie_name = "";
-        private readonly string movie_path = "";
+        private string movie_name = "";
+        private string movie_path = "";
         private readonly long movie_length = 0;
         private readonly long movie_size = 0;
-        private readonly DateTime last_date = DateTime.Now;
-        private readonly DateTime file_date = DateTime.Now;
-        private readonly DateTime regist_date = DateTime.Now;
+        private DateTime last_date = DateTime.Now;
+        private DateTime file_date = DateTime.Now;
+        private DateTime regist_date = DateTime.Now;
         private readonly long score = 0;
-        private readonly long view_count = 0;
+        private long view_count = 0;
         private readonly string hash = "";
         private readonly string container = "";
         private readonly string video = "";
@@ -36,18 +36,22 @@ namespace IndigoMovieManager
         private readonly string comment2 = "";
         private readonly string comment3 = "";
         private readonly double fps = 30;
+        private readonly double totalFrames = 0;
 
-        public MovieInfo(string fileFullPath) {
+        public MovieInfo(string fileFullPath, bool noHash = false)
+        {
             using var capture = new VideoCapture(fileFullPath);
-            var frameCount = capture.Get(VideoCaptureProperties.FrameCount);
+            //なんか、Grabしないと遅いって話をどっかで見たので。
+            capture.Grab();
+            totalFrames = capture.Get(VideoCaptureProperties.FrameCount);
             fps = capture.Get(VideoCaptureProperties.Fps);
-            double durationSec = frameCount / fps;
+            double durationSec = totalFrames / fps;
 
             FileInfo file = new(fileFullPath);
             long size = file.Length;
 
             var iso = capture.Get(VideoCaptureProperties.IsoSpeed);
-            
+
             var now = DateTime.Now;
             var result = now.AddTicks(-(now.Ticks % TimeSpan.TicksPerSecond));
             last_date = result;
@@ -57,26 +61,30 @@ namespace IndigoMovieManager
             movie_path = fileFullPath;
             movie_length = (long)durationSec;
             movie_size = size;
-            hash = Tools.GetHashCRC32(fileFullPath);
+            if (noHash == false)
+            {
+                hash = Tools.GetHashCRC32(fileFullPath);
+            }
 
             var lastWrite = file.LastWriteTime;
             result = lastWrite.AddTicks(-(lastWrite.Ticks % TimeSpan.TicksPerSecond));
             file_date = result;
         }
 
+        //無駄なもんもありつつ、FPSやらは追加してる。
         public long MovieId { get { return movie_id; } set { movie_id = value; } }
-        public string MovieName {  get { return movie_name; } }
-        public string MoviePath { get { return movie_path; } }
+        public string MovieName { get { return movie_name; } set { movie_name = value; } }
+        public string MoviePath { get { return movie_path; } set { movie_path = value; } }
         public long MovieLength { get { return movie_length; } }
         public long MovieSize { get { return movie_size; } }
-        public DateTime LastDate { get { return last_date; } }
-        public DateTime FileDate { get { return file_date; } }
-        public DateTime RegistDate { get { return regist_date; } }
+        public DateTime LastDate { get { return last_date; } set { last_date = value; } }
+        public DateTime FileDate { get { return file_date; } set { file_date = value; } }
+        public DateTime RegistDate { get { return regist_date; } set { regist_date = value; } }
         public long Score { get { return score; } }
-        public long ViewCount { get { return view_count; } }
+        public long ViewCount { get { return view_count; } set { view_count = value; } }
         public string Hash { get { return hash; } }
-        public string Container {  get { return container; } }
-        public string Video {  get { return video; } }
+        public string Container { get { return container; } }
+        public string Video { get { return video; } }
         public string Audio { get { return audio; } }
         public string Extra { get { return extra; } }
         public string Title { get { return title; } }
@@ -86,7 +94,7 @@ namespace IndigoMovieManager
         public string Writer { get { return writer; } }
         public string Genre { get { return genre; } }
         public string Track { get { return track; } }
-        public string Camera {  get { return camera; } }
+        public string Camera { get { return camera; } }
         public string CreateTime { get { return create_time; } }
         public string Kana { get { return kana; } }
         public string Roma { get { return roma; } }
@@ -95,5 +103,6 @@ namespace IndigoMovieManager
         public string Comment2 { get { return comment2; } }
         public string Comment3 { get { return comment3; } }
         public double FPS { get { return fps; } }
+        public double TotalFrames { get { return totalFrames; } }
     }
 }

90ee674e260fd8a4b9cb9ed4141a0ddf2c456a72
dhamaoka <xiacelite@gmail.com>
2024-01-02 10:31:19 +0900

    0.0.0.31 監視のリネーム時処理を更新

---
+++ FileNameValidationRule.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace IndigoMovieManager
+{
+    /// <summary>
+    /// パクリ元。https://araramistudio.jimdo.com/2016/09/16/wpf%E3%81%AE%E5%85%A5%E5%8A%9B%E8%A6%8F%E5%88%B6%E3%82%92%E3%82%AB%E3%82%B9%E3%82%BF%E3%83%9E%E3%82%A4%E3%82%BA/
+    /// </summary>
+    public class FileNameValidationRule : ValidationRule
+    {
+        public bool NotEmpty { get; set; }
+        public string MessageHeader { get; set; }
+
+        public FileNameValidationRule() {
+            NotEmpty = false;
+            MessageHeader = string.Empty;
+        }
+
+        public FileNameValidationRule(bool notEmpty, string messageHeader)
+        {
+            NotEmpty = notEmpty;
+            MessageHeader = messageHeader;
+        }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (null == value)
+            {
+                if (NotEmpty)
+                {
+                    var msg = "値を入力してください。";
+                    if (!string.IsNullOrWhiteSpace(MessageHeader))
+                    {
+                        msg = MessageHeader + "に" + msg;
+                        return new ValidationResult(false, msg);
+                    }
+                }
+                else
+                {
+                    return ValidationResult.ValidResult;
+                }
+            }
+
+            string str = value.ToString();
+            if (string.IsNullOrEmpty(str))
+            {
+                if (NotEmpty)
+                {
+                    var msg = "値を入力してください。";
+                    if (!string.IsNullOrWhiteSpace(MessageHeader))
+                    {
+                        msg = MessageHeader + "に" + msg;
+                        return new ValidationResult(false, msg);
+                    }
+                }
+                else
+                {
+                    return ValidationResult.ValidResult;
+                }
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
---
+++ RenameFile.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace IndigoMovieManager
+{
+    /// <summary>
+    /// RenameFile.xaml の相互作用ロジック
+    /// </summary>
+    public partial class RenameFile : Window
+    {
+        private MessageBoxResult _closeStatus = MessageBoxResult.Cancel;
+
+        public RenameFile()
+        {
+            InitializeComponent();
+            Closing += RenameFile_Closing;
+            ContentRendered += RenameFile_ContentRendered;
+        }
+
+        private void RenameFile_ContentRendered(object sender, EventArgs e)
+        {
+            FileNameEditBox.Focus();
+        }
+
+        private void RenameFile_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(FileNameEditBox.Text))
+            {
+                MessageBox.Show("ファイル名が入力されていません。", Assembly.GetExecutingAssembly().GetName().Name, MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                FileNameEditBox.Focus();
+                e.Cancel = true;
+                return;
+            }
+
+            if (string.IsNullOrEmpty(ExtEditBox.Text)) {
+                MessageBox.Show("拡張子が入力されていません。", Assembly.GetExecutingAssembly().GetName().Name, MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                ExtEditBox.Focus();
+                e.Cancel = true;
+                return;
+            }
+        }
+
+        public MessageBoxResult CloseStatus() { return _closeStatus; }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                _closeStatus = btn.Name switch
+                {
+                    "OK" => MessageBoxResult.OK,
+                    "Cancel" => MessageBoxResult.Cancel,
+                    _ => MessageBoxResult.Cancel,
+                };
+            }
+
+            if (_closeStatus == MessageBoxResult.OK)
+            {
+                if (string.IsNullOrEmpty(FileNameEditBox.Text))
+                {
+                    MessageBox.Show("ファイル名が入力されていません。", Assembly.GetExecutingAssembly().GetName().Name, MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                    FileNameEditBox.Focus();
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(ExtEditBox.Text))
+                {
+                    MessageBox.Show("拡張子が入力されていません。", Assembly.GetExecutingAssembly().GetName().Name, MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                    ExtEditBox.Focus();
+                    return;
+                }
+            }
+
+            Hide();
+        }
+
+        private void ExtEditBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ExtEditBox.SelectAll();
+        }
+
+        private void FileNameEditBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            FileNameEditBox.SelectAll();
+        }
+    }
+}
